35d4dce09950122b06c51810241be93f2b95dac7
Divyansh Vashishtth <divyanshvashishtth123@gmail.com>
2026-07-17 12:34:26 +0530

    update blink blogs

--- BlinkBlog/src/components/index.js
+++ BlinkBlog/src/components/index.js
@@ -4,12 +4,21 @@ import Container from "./container/Container"
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from './RTE.jsx'
-
+import Signup from "./Signup.jsx"
+import Login from "./Login.jsx"
+import Post from "./Post"
+import PostForm from "./Post-form/PostForm.jsx"
+import PostCard from "./PostCard.jsx"
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard
 }
